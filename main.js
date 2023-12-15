7b469e39e2253fe5ca4623066315ef42cea066f0
0800-asd <124334621+0800-asd@users.noreply.github.com>
2023-12-15 17:33:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,12 @@ document.getElementById('uploadForm').addEventListener('submit', async function 
             body: formData,
         });
 
-        const data = await response.json();
-        const downloadLink = document.getElementById('downloadLink');
-        downloadLink.innerHTML = `<p>URL de descarga: <a href="${data.downloadUrl}" target="_blank">${data.downloadUrl}</a></p>`;
+        // Si la respuesta es una redirección, actualizar la página con la nueva URL
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else {
+            console.error('Error al subir el archivo:', response.statusText);
+        }
     } catch (error) {
         console.error('Error al subir el archivo:', error);
     }
